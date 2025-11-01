5953eeca120f6f6f1586a9896b4ff8c542603955
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-01 20:07:13 +0600

    add Gallery component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Gallery from "./components/Gallery"
+
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Gallery />
     </>
   )
 }
