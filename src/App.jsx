0fc66085f4df792aca9c9bae04473662498e606c
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-04 18:52:51 +0600

    Find the winner

--- src/App.jsx
+++ src/App.jsx
@@ -26,11 +26,20 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+  let status;
+
+  if(winner){
+    status = `Winner: ${winner}`;
+  } else{
+    status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
   function onSquareClick(i){
     const nextSquares = squares.slice(); //? create a copy of the squares because state is immutable. I want keep the main state unchanged.
 
     // if the square is already filled then it will disable
-    if(nextSquares[i]){
+    if(nextSquares[i] || calculateWinner(squares)){
       return;
     }
 
@@ -52,6 +61,7 @@ export default function Board() {
 
   return (
     <>
+    <div>{status}</div>
       <div className="flex">
         <Square value={squares[0]} onSquareClick={() => onSquareClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => onSquareClick(1)}/>
@@ -71,4 +81,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for( let i=0; i<lines.length; i++ ){
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c])
+    {
+      return squares[a];
+    }
+  }
+  return null;
 }
