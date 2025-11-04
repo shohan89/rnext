6c228cf3188b8bb26a3346b500ad565fd8368559
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-04 13:26:34 +0600

    add click handler to handel click

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,17 @@
 */
 
 function Square({ value }){
-  return <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
+  function handleClick() {
+    console.log('Button clicked!')
+  }
+  return (
+  <button 
+  className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg cursor-pointer"
+  onClick={handleClick}
+  >
+    {value}
+  </button>
+);
 }
 
 export default function Board() {
