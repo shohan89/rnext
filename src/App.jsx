fd8a0b0008f0be941ff48e6d1da0e5064d86f13e
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-04 13:15:58 +0600

    define Square component and use it to Board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+/* Component Hierarchy:
 
+    Game
+      => Board
+        => Square
+      => History
 
-export default function App() {
+*/
+
+function Square({ value }){
+  return <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
+}
+
+export default function Board() {
 
   return (
-    <h2 className="text-3xl font-bold underline">Tic Tac Toe</h2>
+    <>
+      <div>
+        <Square value={1}/>
+        <Square value={2}/>
+        <Square value={3}/>
+      </div>
+
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
