563ec3a77f6b6af3d691ea05fba04f79988e4751
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-16 19:20:39 +0600

    5 steps of UI declarative

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import Form from "./components/Form";
 
 export default function App() {
-  const statuses = ["empty", "typing", "submitting", "error", "success"];
   return (
     <div>
-      {statuses.map((status) => (
-        <Form key={status} status={status} />
-      ))}
+        <Form/>
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,56 @@
-export default function Form({ status }) {
-  if (status === "success") return <h1>That's right!</h1>;
+import { useState } from "react";
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (answer.toLowerCase() === 'dhaka') {
+        resolve();
+      } else {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      }
+    }, 3000);
+  });
+}
+
+
+export default function Form() {
+    // visual states "empty", "typing", "submitting", "error", "success"
+    // mandatory state
+    const [ answer, setAnswer ] = useState("");
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState("typing");
+
+    // handlers
+    function handleTextAreaChange(event){
+        setAnswer(event.target.value);
+        setError(null);
+    }
+
+    async function handleFormSubmit(e){
+        e.preventDefault();
+        try {
+            await submitForm(answer);
+            setStatus('submitting');
+        } catch (err) {
+            setStatus('typing');
+            setError(err)
+        }
+    }
+  if ( status === 'success' ) return <h1>That's right!</h1>;
   return (
     <>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <h2>City quiz</h2>
         <p>What city is located on two continents?</p>
-        <textarea disabled={status === "submitting"}></textarea>
+        <textarea value={answer} onChange={handleTextAreaChange}></textarea>
         <br />
-        <button disabled={status === "submitting" || status === "empty"}>
+        <button disabled={ status === 'submitting' || answer === '' }>
           Submit
         </button>
-        {status === "submitting" && <p>Loading...</p>}
-        {status === "error" && <p>There was an error</p>}
+        { status === "submitting" && <p>Loading...</p> }
+        { error && <p style={{ color: "red" }}>{error.message}</p> }
       </form>
-      <hr />
     </>
   );
 }
