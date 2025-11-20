02ef17b95a06235d7c8febd9beb2ac7c60d0a594
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-20 17:58:59 +0600

    context passes through intermediate components

---
+++ src/components/RecentPosts.jsx
@@ -0,0 +1,19 @@
+import Heading from "./Heading";
+import Post from "./Post";
+import Section from "./Section";
+
+export default function RecentPosts() {
+  return (
+    <Section>
+      <Heading>Recent Posts</Heading>
+      <Post
+        title="Flavors of Lisbon"
+        body="...those pastéis de nata!"
+      />
+      <Post
+        title="Buenos Aires in the rhythm of tango"
+        body="I loved it!"
+      />
+    </Section>
+  );
+}
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,19 @@
 import { useContext } from 'react';
 import { LevelContext } from '../contexts/LevelContext';
 import '../index.css';
-export default function Section({ children }) {
-    const level = useContext(LevelContext);
+
+
+export default function Section({ children, isFancy }) {
+  const level = useContext(LevelContext);
   return (
-    <section className="section">
-      <LevelContext.Provider value={level + 1}>
+    <section className={
+      'section ' +
+      (isFancy ? 'fancy' : '')
+    }>
+      <LevelContext value={level + 1}>
         {children}
-      </LevelContext.Provider>
+      </LevelContext>
     </section>
   );
 }
+
