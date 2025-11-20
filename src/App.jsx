ebb312ee5609575452a87a4fa4027766dcdc21d3
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-20 13:54:19 +0600

    Passing data deeoly with Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,25 @@
-import { useState } from "react";
-import AddTask from "./components/AddTask";
-import TaskList from "./components/TaskList";
-import { initialTasks } from "./data/tasks";
+import Heading from './components/Heading';
+import Section from './components/Section';
 
-export default function App() {
-  const [tasks, setTasks] = useState(initialTasks);
-
-  // get next id
-  function getNextId(tasks){
-    const maxId = tasks.reduce(( prev, current ) => prev && prev.id > current.id ? prev.id : current.id);
-    return maxId + 1;
-  }
-
-  // handlers
-  function handleAddTask(newTaskText){
-    setTasks([
-      ...tasks,
-      {id: getNextId(tasks), text: newTaskText, done: false}
-    ])
-  }
-  
-  function handleUpdateTask(updatedTask){
-    const nextTasks = tasks.map( task => {
-      if( task.id === updatedTask.id ){
-        return updatedTask;
-      } else {
-        return task;
-      }
-    } )
-    setTasks(nextTasks);
-  }
-
-  function handleDeleteTask(deletedTaskId){
-    const nextTasks = tasks.filter( task => task.id !== deletedTaskId );
-    setTasks(nextTasks);
-  }
-
+export default function Page() {
   return (
-    <>
-        <h1>To Do List</h1>
-        <AddTask onAddTask={handleAddTask}/>
-        <TaskList tasks={tasks} onUpdateTask={handleUpdateTask} onDeleteTask={handleDeleteTask}/>
-    </>
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
   );
 }
