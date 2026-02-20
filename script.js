f2e0dc127b73a3e5fff07b48eadf32ea1ffd1f65
shohan89 <your-maffin.killer007@gmail.com>
2026-02-21 01:37:21 +0600

    Add event listener to add to cart buton and update the ui on click.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Connected!!');
+// DOM elements
+const price = document.getElementById('price');
+const total = document.getElementById('total');
+console.log("🚀 ~ total:", total)
+const addToCartBtn = document.getElementById('addToCartBtn');
+
+
+// State or data
+const productPrice = 5000;
+let totalPrice = 0;
+
+// Set product price initially
+price.innerText = `৳ ${productPrice}`;
+
+// Set total price initially
+total.innerText = `৳ ${totalPrice}`;
+
+// Add event listener
+addToCartBtn.addEventListener('click', () => {
+    totalPrice += productPrice;
+
+    // Update the UI
+    total.innerText = `৳ ${totalPrice}`;
+})
