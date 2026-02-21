bf07c097f2c48be4a3eb1f749d0b2d08a5584355
shohan89 <your-maffin.killer007@gmail.com>
2026-02-22 01:27:46 +0600

    Create add to cart functionality using React core concepts.

--- reactScript.js
+++ reactScript.js
@@ -6,10 +6,45 @@
 // root.appendChild(p);
 
 // How React works under the hood
-const myElement = (
-    <div>
-        <h1>Hello from React!</h1>
-        <p>Transpiled with Babel!</p>
+const price = 5000;
+const Product = () => {
+    const [quantity, setQuantity] = React.useState(0);
+    const [total, setTotal] = React.useState(0);
+    // Update total price when quantity changes
+    const addToCart = () => {
+        setQuantity(quantity + 1);
+        setTotal(total + price);
+    }
+    return (
+        <div className="shadow-md rounded-lg bg-slate-800 border-slate-700">
+      <a href="#">
+        <img
+          className="rounded-t-lg"
+          src="https://dummyimage.com/680X400/087ea4/ffffff.png&text=Product"
+          alt="product image"
+        />
+      </a>
+      <div className="px-5 py-5">
+        <div className="flex items-center justify-between mt-5">
+          <a href="#">
+            <h3 className="font-semibold text-xl tracking-tight">
+              Reactive Accelerator Course
+            </h3>
+          </a>
+          <span id="total" className="text-xl font-medium text-teal-500">৳ {total}</span>
+        </div>
+        <div className="flex items-center justify-between mt-5">
+          <span id="price" className="text-2xl font-bold text-slate-300">৳ {price}</span>
+          <a
+            onClick={addToCart}
+            href="#"
+            className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-[#087ea4] hover:bg-[#087ea4]/[.8] focus:ring-[#087ea4]/[.5]"
+            >Add to cart</a
+          >
+        </div>
+      </div>
     </div>
-);
-ReactDOM.createRoot(document.getElementById('root')).render(myElement);
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Product />);
