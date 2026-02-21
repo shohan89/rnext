b2dbe73e4a1343dec5296d7220a0aecb31bfc67c
shohan89 <your-maffin.killer007@gmail.com>
2026-02-22 00:29:50 +0600

    Difference between how DOM & React DOM Works under the hood.

--- reactScript.js
+++ reactScript.js
@@ -1,2 +1,10 @@
-console.log(React);
-console.log(ReactDOM);
+// This is JS Dom
+// const p = document.createElement('p');
+// p.innerText = 'This is a React component rendered in a non-React environment.';
+
+// const root = document.getElementById('root');
+// root.appendChild(p);
+
+// How React works under the hood
+const p = React.createElement('p', null, 'Hello from React!');
+ReactDOM.createRoot(document.getElementById('root')).render(p);
