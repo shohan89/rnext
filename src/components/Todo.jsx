75ac6deab8d78d8056271b6ca672ab76773474bf
shohan89 <your-maffin.killer007@gmail.com>
2026-02-24 22:12:36 +0600

    Write some expression

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,13 +8,34 @@ function formatDate(date) {
         day: 'numeric'
     }).format(date);
 }
+// person object
+const person = {
+  name: 'Gregorio Y. Zara',
+  profile: "https://i.imgur.com/7vQD0fPs.jpg",
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
 
 export default function TodoList() {
-  const name = 'Hedy Lamarr';
   return (
     <>
-        <h1>{name}'s To Do List for {formatDate(today)}!</h1>
+        <h1>{person.name}'s To Do List for {formatDate(today)}!</h1>
         <h3>Current Time: {today.toLocaleString()}</h3>
+        <div style={person.theme}>
+            <h1>{person.name}'s Todos</h1>
+            <img
+                className="avatar"
+                src={person.profile}
+                alt="Gregorio Y. Zara"
+            />
+            <ul>
+                <li>Improve the videophone</li>
+                <li>Prepare aeronautics lectures</li>
+                <li>Work on the alcohol-fuelled engine</li>
+            </ul>
+        </div>
     </>
   );
 }
