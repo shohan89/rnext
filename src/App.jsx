29626cb7cbee829ffc696026cdf0a38b83c6fad8
shohan89 <your-maffin.killer007@gmail.com>
2026-02-28 21:40:11 +0600

    Redering List using map & filter and understand the concept of using key.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import PackingList from "./components/PackingList";
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
 
 function App() {
+  const chemists = people
+    .filter((person) => person.profession === "chemist")
+    .map((chemist) => <li key={chemist.id}>{chemist.name}</li>);
   return (
     <div>
-      {/* <Profile>
-        <Avatar person={{ name: "shohan" }} size={100} />
-        <Avatar person={{ name: "Sharoar" }} size={80} />
-      </Profile> */}
-      {/* <Gallery /> */}
-      {/* <Profile /> */}
-      <PackingList />
+      <ul>{chemists}</ul>
     </div>
   );
 }
