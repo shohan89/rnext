35ce321e98ab06508e0f4ad2ec61c80c2b79ebea
shohan89 <your-maffin.killer007@gmail.com>
2026-03-05 03:23:37 +0600

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import "./App.css";
  *
  */
 
-function Square() {
+function Square({ value }) {
   return (
     <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-      X
+      {value}
     </button>
   );
 }
@@ -22,19 +22,19 @@ function Board() {
   return (
     <>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </>
   );
