45afb1c41556798c0106e3680457d853f7d98924
shohan89 <your-maffin.killer007@gmail.com>
2026-03-06 02:11:37 +0600

    Make all square clickable on Board Component

--- src/App.jsx
+++ src/App.jsx
@@ -24,27 +24,28 @@ function Square({ value, onSquareClick }) {
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  function handleSquareClick() {
+  function handleSquareClick(squareIdx) {
     const nextSquare = squares.slice();
-    nextSquare[0] = "X";
+
+    nextSquare[squareIdx] = "X";
     setSquares(nextSquare);
   }
   return (
     <>
       <div className="flex">
-        <Square value={squares[0]} onSquareClick={handleSquareClick} />
-        <Square value={squares[1]} onSquareClick={handleSquareClick} />
-        <Square value={squares[2]} onSquareClick={handleSquareClick} />
+        <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)} />
       </div>
       <div className="flex">
-        <Square value={squares[3]} onSquareClick={handleSquareClick} />
-        <Square value={squares[4]} onSquareClick={handleSquareClick} />
-        <Square value={squares[5]} onSquareClick={handleSquareClick} />
+        <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)} />
       </div>
       <div className="flex">
-        <Square value={squares[6]} onSquareClick={handleSquareClick} />
-        <Square value={squares[7]} onSquareClick={handleSquareClick} />
-        <Square value={squares[8]} onSquareClick={handleSquareClick} />
+        <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)} />
       </div>
     </>
   );
