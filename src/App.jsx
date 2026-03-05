4c2468e8b6c725e65a432c1873fade294c0b5313
shohan89 <your-maffin.killer007@gmail.com>
2026-03-05 22:13:49 +0600

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 // Component Heirarchy
@@ -10,31 +11,40 @@ import "./App.css";
  *
  */
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   return (
-    <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
+    <button
+      onClick={onSquareClick}
+      className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg"
+    >
       {value}
     </button>
   );
 }
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleSquareClick() {
+    const nextSquare = squares.slice();
+    nextSquare[0] = "X";
+    setSquares(nextSquare);
+  }
   return (
     <>
-      <div>
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={handleSquareClick} />
+        <Square value={squares[1]} onSquareClick={handleSquareClick} />
+        <Square value={squares[2]} onSquareClick={handleSquareClick} />
       </div>
-      <div>
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={handleSquareClick} />
+        <Square value={squares[4]} onSquareClick={handleSquareClick} />
+        <Square value={squares[5]} onSquareClick={handleSquareClick} />
       </div>
-      <div>
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={handleSquareClick} />
+        <Square value={squares[7]} onSquareClick={handleSquareClick} />
+        <Square value={squares[8]} onSquareClick={handleSquareClick} />
       </div>
     </>
   );
