8d59dc18858fea9699f0f285d2a1ef75a2e2e44d
shohan89 <your-maffin.killer007@gmail.com>
2026-03-11 01:54:18 +0600

    Stop event propagation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
-}
-
-function PlayButton({ movieName }) {
-  function handlePlayClick() {
-    alert(`Playing ${movieName}!`);
-  }
-
-  return <Button onClick={handlePlayClick}>Play "{movieName}"</Button>;
-}
-
-function UploadButton() {
-  return <Button onClick={() => alert("Uploading!")}>Upload Image</Button>;
+function Button({ onBtnClick, children }) {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onBtnClick();
+      }}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default function Toolbar() {
   return (
-    <div>
-      <PlayButton movieName="Kiki's Delivery Service" />
-      <UploadButton />
+    <div
+      className="Toolbar"
+      onClick={() => {
+        alert("You clicked on the toolbar!");
+      }}
+    >
+      <h2>This is Toolbar</h2>
+      <Button onBtnClick={() => alert("Playing!")}>Play Movie</Button>
+      <Button onBtnClick={() => alert("Uploading!")}>Upload Image</Button>
     </div>
   );
 }
